effb713d48c1cf66a4cad1f6b8e2ebb453427517
“sukyung6999” <sukyung6999@gmail.com>
2023-10-20 20:07:58 +0900

    add: 코드팩토리 1일차

---
+++ codefactory/1_basics/3_declaring_variable.js
@@ -0,0 +1,41 @@
+/**
+ * Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다
+ * 2) let
+ * 3) const
+ */
+var name = '코드팩토리';
+console.log(name);
+
+var age = 27;
+console.log(age);
+
+let ive = '아이브';
+console.log(ive);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다
+ */
+ive = '안유진';
+console.log(ive);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '코드팩토리';
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것
+ * 2) 할당 
+ */
+var name = '아아'; 
+console.log(name);
+
+let girlfriend;
+console.log(girlfriend);
+
+// const girlfriend2;
