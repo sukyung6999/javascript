35f8b56152e2aa304ab097d216a51184505876a5
“sukyung6999” <sukyung6999@gmail.com>
2023-11-05 12:34:11 +0900

    practice: [프로젝트101] 섹션 6. 날씨 앱 (API)

---
+++ project101/06-weather-app/practice.js
@@ -0,0 +1,40 @@
+import {API_KEY} from './env.js';
+
+const assignWeather = (data) => {
+  const img = document.querySelector('.icon');
+  const temp = document.querySelector('.temp');
+  const weather = document.querySelector('.weather');
+  const city = document.querySelector('.city');
+
+  img.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+  temp.innerText = `${data.main.temp}°C`;
+  weather.innerText = data.weather[0].description;
+  city.innerText = data.name;
+}
+
+const getCurrentWeather = (latitude, longitude) => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`;
+
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      assignWeather(data);
+    });
+}
+
+const getPosition = (position) => {
+  const {latitude, longitude} = position.coords;
+
+  getCurrentWeather(latitude, longitude);
+}
+
+const errorHandler = () => {
+  const noti = document.querySelector('.noti');
+
+  noti.style.display = 'block';
+}
+
+if ("geolocation" in navigator) {
+  navigator.geolocation.getCurrentPosition(getPosition, errorHandler);
+} 
